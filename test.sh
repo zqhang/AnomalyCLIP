cc0cfbad08ab76e881bc7e1dcfbc3f91e252fb0c
Qihang Zhou <95111581+zqhang@users.noreply.github.com>
2025-04-13 11:46:47 +0800

    AnomalyCLIP uses only  top feature map for anomaly segmentation

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ for i in "${!depth[@]}";do
         CUDA_VISIBLE_DEVICES=${device} python test.py --dataset mvtec \
         --data_path /remote-home/iot_zhouqihang/data/mvdataset --save_path ./results/${base_dir}/zero_shot \
         --checkpoint_path ${save_dir}epoch_15.pth \
-         --features_list 6 12 18 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]}
+         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]}
     wait
     done
 done
@@ -33,7 +33,7 @@ for i in "${!depth[@]}";do
         CUDA_VISIBLE_DEVICES=${device} python test.py --dataset visa \
         --data_path /remote-home/iot_zhouqihang/data/Visa --save_path ./results/${base_dir}/zero_shot \
         --checkpoint_path ${save_dir}epoch_15.pth \
-        --features_list 6 12 18 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]}
+        --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]}
     wait
     done
 done
